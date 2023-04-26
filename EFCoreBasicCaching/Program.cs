5bd7c5a8e9641d3941caf5adcf2d3d4d2d1f0209
HumairaSid <131204413+HumairaSid@users.noreply.github.com>
2023-04-25 20:03:26 -0700

    EFCore SQLLite, This solution has errors

--- EFCoreBasicCaching/Program.cs
+++ EFCoreBasicCaching/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace EFCoreBasicCaching
+﻿using System.Reflection.Metadata;
+
+namespace EFCoreBasicCaching
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Menu SQlLite");
+            using var db= new RestaurantContext();
+            // Note: This sample requires the database to be created before running.
+            Console.WriteLine($"Database path: {db.DbPath}.");
+            // Create a menu
+            Console.WriteLine("Inserting a new Menu");
+            db.Add(new Menu {MenuId= 1, MenuName = "Chilly Chicken", MenuDescription = "Spicy chicken, fried with bell peppers in hot garlic sauce." });
+            db.SaveChanges();
+
+            // Create a Order
+            Console.WriteLine("Inserting a new Order");
+            db.Add(new Order { OrderDate = DateTime.Now, MenuId = 1, TotalPrice = 20 });
+            db.SaveChanges();
+            Console.WriteLine("Test");
+
         }
+
+
     }
 }
